25817d6d81108648d49a59235c7b63b69da9ccfc
Antonio Drusin <antonio.drusin@gmail.com>
2024-04-15 20:55:10 -0400

    Add script

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Personal Aliases
 
+## Create torrent machine in aws
+
+See torrent-setup.sh
+
+
 ## addpush
 echo "alias addpush='git add -A && git commit --amend --no-edit && git push --force'" >> ~/.bashrc
 source ~/.bashrc
---
+++ torrent-setup.sh
@@ -0,0 +1,16 @@
+﻿# https://github.com/transmission/transmission/blob/main/docs/Building-Transmission.md
+# 1GB is enough to compile on intel, compilation for ARM will need 2GB of RAM 
+sudo yum install cmake gcc gcc-c++ m4 make automake libtool gettext openssl-devel git libcurl-devel pkgconf-pkg-config intltool -y
+git clone --recurse-submodules https://github.com/transmission/transmission Transmission
+cd Transmission
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cd build
+cmake --build .
+sudo cmake --install .
+mkdir ~/Downloads
+transmission-daemon
+# Adding a torrent
+transmission-remote -a "magnet:?xt=urn:btih:857A1347AEB7AE2CE0569A5CAC9E0CA92E51279A&dn=amigavision-2024&tr=http%3a%2f%2fbt1.archive.org%3a6969%2fannounce&tr=http%3a%2f%2fbt2.archive.org%3a6969%2fannounce&tr=wss%3a%2f%2fwstracker.online&ws=https%3a%2f%2farchive.org%2fdownload%2f&ws=http%3a%2f%2fia600309.us.archive.org%2f33%2fitems%2f&ws=http%3a%2f%2fia800309.us.archive.org%2f33%2fitems%2f"
+# Viewing status
+transmission-remote -l
+
